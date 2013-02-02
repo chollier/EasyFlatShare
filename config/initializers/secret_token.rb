fb60514b2edb0d63e430d29bed57deea7e2d4b57
Loïc CHOLLIER <loic@chollier.com>
2013-02-02 14:30:11 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyFlatFees::Application.config.secret_token = 'a7ca4298ef772594d1ec859093f2d7736dd08a943eca2ec5d3d7398eb3c5b4985a2cbeb3fff1a8d532af652ef0365a91b895f0aeea4551dd044de2b880dc7a71'
